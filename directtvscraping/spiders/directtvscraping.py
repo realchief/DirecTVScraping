25e2b82b7c3374840162ed306f303c8280aadb84
realchief <stryokka@gmail.com>
2018-03-16 23:20:34 +0100

    latest result

--- directtvscraping/spiders/directtvscraping.py
+++ directtvscraping/spiders/directtvscraping.py
@@ -1,34 +1,28 @@
-from scrapy.conf import settings
-from urllib import urlencode
 from scrapy import Request
 import scrapy
-import re
+from scrapy.log import WARNING
 import json
 import csv
 import os
-import time
 import traceback
-import requests
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import WebDriverException
-from pyvirtualdisplay import Display
-from selenium import webdriver
-import socket
 
-CWD = os.path.dirname(os.path.abspath(__file__))
-# driver_path = os.path.join(CWD, 'bin', 'chromedriver')
-# driver_log_path = os.path.join(CWD, 'bin', 'driver.log')
-
-keys = []
+zip_codes = []
+Channels = []
+Genere = []
+Ott = []
+Popular = []
+Tags = []
+ShortName = []
+Alternative_Name = []
+Top_Shows = []
+Channel_Description = []
+Conversion_Names = []
 
 try:
     with open(os.path.abspath('DirecTV_input.csv'), 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            keys.append(row[2])
+            zip_codes.append(row[2])
 
 except Exception as e:
     print('parse_csv Function => Got Error: {}'.format(e))
@@ -37,59 +31,125 @@ except Exception as e:
               '/inputdata/DirecTV_input.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            keys.append(row[2])
+            zip_codes.append(row[2])
+
+try:
+    with open(os.path.abspath('DirecTV_input.csv'), 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            Channels.append(row[0])
+            Genere.append(row[1])
+            Ott.append(row[2])
+            Popular.append(row[3])
+            Tags.append(row[4])
+            ShortName.append(row[5])
+            Alternative_Name.append(row[6])
+            Top_Shows.append(row[7])
+            Channel_Description.append(row[8])
+            Conversion_Names.append(row[9])
+
+except Exception as e:
+    print('parse_csv Function => Got Error: {}'.format(e))
+
+    with open('/home/ubuntu/Stefan-Scrapy-DiscoveryMarkets/DiscoveryMarketsScraping/DirectTV'
+              '/directtvscraping/inputdata/Channels.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            Channels.append(row[0])
+            Genere.append(row[1])
+            Ott.append(row[2])
+            Popular.append(row[3])
+            Tags.append(row[4])
+            ShortName.append(row[5])
+            Alternative_Name.append(row[6])
+            Top_Shows.append(row[7])
+            Channel_Description.append(row[8])
+            Conversion_Names.append(row[9])
 
 
 class SiteProductItem(scrapy.Item):
-    container_num = scrapy.Field()
+    Channel = scrapy.Field()
+    package_name = scrapy.Field()
+    zip_code = scrapy.Field()
+    Genre = scrapy.Field()
+    OTT = scrapy.Field()
+    Popular = scrapy.Field()
+    Tags = scrapy.Field()
+    ShortName = scrapy.Field()
+    AlternateNames = scrapy.Field()
+    TopShows = scrapy.Field()
+    ChannelDescription = scrapy.Field()
+    ConversionNames = scrapy.Field()
 
 
-class DirecTV (scrapy.Spider):
-
-    name = "scrapingdata"
-    allowed_domains = ['www.att.com']
+class ATTProductsSpider (scrapy.Spider):
+    name = "att_products"
+    allowed_domains = ['att.com']
 
     start_urls = [
-        'https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do']
+        'https://www.att.com/channellineup/tv/tvchannellineup.html?tvType=directv&cta_button=AddToCart&pricing_terms'
+        '=true&lang=en']
+
+    PROD_URL = "https://www.att.com/apis/channellineup/getChannelData?_=1521230061137"
 
     def start_requests(self):
-        for start_url in self.start_urls:
+        for zipCode in zip_codes[1:]:
+            yield Request(
+                url=self.PROD_URL,
+                callback=self.parse_product,
+                method="POST",
+                body=json.dumps({"county": "SINGLE_COUNTY", "isLatino": "false", "tvType": "dtv", "zipCode": zipCode}),
+                dont_filter=True,
+                meta={'zip_code': zipCode},
+                headers={
+                    "content-type": "application/json",
+                    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                  "Chrome/64.0.3282.186 Safari/537.36",
 
-            if 'www.shipmentlink.com' in start_url:
-                for key in keys_EMC:
-                    form_data = {'CNTR': key, 'TYPE': 'CNTR', 'blFields': '1', 'cnFields': '1', 'is-quick': 'Y'}
-                    yield Request(url=start_url,
-                                  headers={
-                                            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                            'Chrome/64.0.3282.186 Safari/537.36',
-                                            'Content-Type': 'application/x-www-form-urlencoded'
-                                           },
-                                  callback=self.parse_product,
-                                  method='POST',
-                                  body=urlencode(form_data),
-                                  dont_filter=True)
+                }
+            )
 
     def parse_product(self, response):
-
-        prod_item = SiteProductItem()
-
-        prod_item['container_num'] = self._parse_ContainerNumber(response)
-
-        if any(value for value in prod_item.values()):
-            return prod_item
-
-    @staticmethod
-    def _parse_ContainerNumber(response):
+        final_channels = []
+        product = SiteProductItem()
+        zip_code = response.meta['zip_code']
+        channel_data = []
+        package_data = []
+        channel_list = []
+        package_list = []
         try:
-            ContainerNumber = response.xpath('//table[@width="95%"][3]/tr[3]/td[1]//text()').extract()
-            if ContainerNumber:
-                ContainerNumber = str(ContainerNumber[0])
-            if not ContainerNumber and response.xpath('//td[@class="bor_L_none"]/a/font/u//text()'):
-                ContainerNumber = str(response.xpath('//td[@class="bor_L_none"]/a/font/u//text()')[0].extract())
-            if not ContainerNumber and response.xpath('//span[@class="tracking_container_id"]//text()'):
-                ContainerNumber = str(response.xpath('//span[@class="tracking_container_id"]//text()')[0].extract())
-            if not ContainerNumber and response.xpath('//tr[@class="field_odd"]//text()').extract():
-                ContainerNumber = str(response.xpath('//tr[@class="field_odd"]//text()').extract()[3])
-            return ContainerNumber if ContainerNumber else None
-        except Exception as e:
-            print('No Data')
+            data = json.loads(response.body).get('channelLineupDetails')
+            channel_data = data.get('channelGroups')
+            package_data = data.get('packages')
+        except:
+            self.log(
+                "Failed parsing json at {} - {}".format(response.url, traceback.format_exc())
+                , WARNING)
+
+        for channel in channel_data:
+            channel_list.append(channel.get('sortName'))
+        for package in package_data:
+            package_list.append(package.get('packageName'))
+
+        for channel in channel_list:
+            for i, c in enumerate(Channels):
+                if channel.lower() == c.lower():
+                    final_channels.append(c)
+
+        for channel in final_channels:
+            for i, c in enumerate(Channels):
+                if channel.lower() == c.lower():
+                    product['Channel'] = c
+                    product['package_name'] = package_list
+                    product['zip_code'] = zip_code
+                    product['Genre'] = Genere[i]
+                    product['OTT'] = Ott[i]
+                    product['Popular'] = Popular[i]
+                    product['Tags'] = Tags[i]
+                    product['ShortName'] = ShortName[i]
+                    product['AlternateNames'] = Alternative_Name[i]
+                    product['TopShows'] = Top_Shows[i]
+                    product['ChannelDescription'] = Channel_Description[i]
+                    product['ConversionNames'] = Conversion_Names[i]
+
+                    yield product
